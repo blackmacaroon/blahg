b4e5f9da570d44da18db8fe1a7c14ef1c4dbecc9
Kayla <6lackmacaroon@gmail.com>
2020-06-23 11:33:14 -0600

    added dynamic routes, troubleshoot isSerialized parsing error

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
 
 export default function Home({ allPostsData }) {
 return (
@@ -24,15 +17,26 @@ return (
       <ul className={utilStyles.list}>
         {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem} key={id}>
-            {title}
-            <br />
-            {id}
-            <br />
-            {date}
-          </li>
+          <Link href="/posts/[id]" as={`/posts/${id}`}>
+            <a>{title}</a>
+          </Link>
+          <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+        </li>
         ))}
       </ul>
     </section>
   </Layout>
 )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
